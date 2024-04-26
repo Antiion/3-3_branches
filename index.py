0f988f25001387dd3b1d6e6e26189ca7b059d1ae
Gotgilf A <gotgilf92@gmail.com>
2024-04-26 15:25:40 +0300

    delivery point

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import tkinter
 window = tkinter.Tk()
-label = tkinter.Label(window, text = 'Текст в окне')
+label = tkinter.Label(window, text = 'Выберите пункт доставки')
 label.pack()
 window.mainloop()
